0abbd71b8e4f4d80d061c661e65b071de7a4a2bc
Ismael Amezcua <amezcua.ismael@gmail.com>
2022-05-15 19:39:44 -0500

    fix(home): escape  with

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ const Home: NextPage = () => {
     <>
       <div className="flex justify-center items-center h-screen w-screen">
         <div className="text-center">
-          <p className="font-bold text-slate-800">Ismael Amezcua's personal website</p>
+          <p className="font-bold text-slate-800">Ismael Amezcua&apos;s personal website</p>
           <p className="text-slate-700">Coming soon</p>
         </div>
       </div>
