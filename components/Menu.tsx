915c51ab2a300bdcec578b1878023cbe0ada607b
Ismael Amezcua <amezcua.ismael@gmail.com>
2022-07-10 22:59:52 -0500

    feat: add icons to the menu

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -1,48 +1,57 @@
 import { FC, ReactElement } from 'react';
 import Link from 'next/link';
+import {
+  DocumentTextIcon,
+  BriefcaseIcon,
+  AcademicCapIcon,
+  ChevronRightIcon,
+} from '@heroicons/react/outline';
 
 const Menu: FC = (): ReactElement => {
   const menuEntries = [
     {
       label: 'Resume',
       description: 'Directly download my CV in PDF format.',
-      icon: 'icon',
+      icon: <DocumentTextIcon className="w-8 h-8" />,
       href: '/iav-resume.pdf',
-      hoverColor: 'group-hover:text-red-600',
+      textColor: 'text-purple-600',
+      bgColor: 'bg-purple-100',
     },
     {
       label: 'Work',
       description: 'Check my personal portfolio.',
-      icon: 'icon',
+      icon: <BriefcaseIcon className="w-8 h-8" />,
       href: '/work',
-      hoverColor: 'group-hover:text-sky-600',
-    },
+      textColor: 'text-pink-600',
+      bgColor: 'bg-pink-100',
+     },
     {
       label: 'Academic profile',
       description: 'See my academic profile page.',
-      icon: 'icon',
+      icon: <AcademicCapIcon className="w- h-8" />,
       href: '/academic-profile',
-      hoverColor: 'group-hover:text-indigo-600',
-    },
+      textColor: 'text-amber-600',
+      bgColor: 'bg-amber-100',
+     },
   ];
 
   return (
     <div className="container mx-auto lg:max-w-3xl">
-      {menuEntries.map(({ label, description, icon, href, hoverColor = '' }, index) => (
+      {menuEntries.map(({ label, description, icon, href, textColor, bgColor }, index) => (
         <Link key={index} href={href}>
           <a>
             <div className="flex flex-row p-6 group first:border-t border-b border-slate-100 hover:bg-slate-50 items-center">
-              <div className="px-6">
+              <div className={`p-2 mx-4 rounded-lg ${textColor} ${bgColor}`}>
                 {icon}
               </div>
 
               <div className="grow">
-                <p className={`lg:text-xl text-slate-800 ${hoverColor}`}>{label}</p>
+                <p className={`lg:text-xl text-slate-800`}>{label}</p>
                 <p className="text-slate-600">{description}</p>
               </div>
 
               <div className="px-6">
-                C
+                <ChevronRightIcon className="w-5 h-5" />
               </div>
             </div>
           </a>
