4a78078a2cccab3f9ab454829f1fff50e6604872
Ismael Amezcua <amezcua.ismael@gmail.com>
2022-07-10 22:34:53 -0500

    feat: add framer motion animations

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,31 @@
 import type { NextPage } from 'next';
 import Link from 'next/link';
+import { motion } from 'framer-motion';
 import Menu from '../components/Menu';
 
 const Home: NextPage = () => {
   return (
     <>
-      <nav className="container mx-auto px-4 py-6">
+      <motion.nav
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 1 }}
+        className="container mx-auto px-4 py-6"
+      >
         <Link href="/">
           <a>
             <h2 className="font-semibold text-lg">Ismael Amezcua.</h2>
           </a>
         </Link>
-      </nav >
+      </motion.nav>
 
       <section className="flex flex-col">
-        <div className="container mx-auto px-4 py-10 mb-10">
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 1, delay: 0.8 }}
+          className="container mx-auto px-4 py-10 mb-10"
+        >
           <h1 className="text-2xl leading-relaxed lg:leading-relaxed text-stone-600 cursor-pointer lg:text-4xl">
             <span className="hover:text-red-600 delay-100 ease-linear transition-colors">
               The non-unique identifier is Ismael Amezcua.{` `}
@@ -32,9 +43,15 @@ const Home: NextPage = () => {
               I do research in my spare time on Wireless Sensor Networks, Industrial Internet of Things, communication protocols, and Web technologies.{` `}
             </span>
           </h1>
-        </div>
+        </motion.div>
 
-        <Menu />
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 1, delay: 1.6 }}
+        >
+          <Menu />
+        </motion.div>
       </section>
     </>
   )
