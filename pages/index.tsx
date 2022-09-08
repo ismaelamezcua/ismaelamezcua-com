a9b6651e74dde56913ef8a64e91719af466cc888
Ismael Amezcua <amezcua.ismael@gmail.com>
2022-09-08 11:00:51 -0500

    feat: refactor landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,60 +1,48 @@
 import type { NextPage } from 'next';
 import Link from 'next/link';
 import { motion } from 'framer-motion';
-import Menu from '../components/Menu';
 
 const Home: NextPage = () => {
   return (
-    <>
-      <motion.nav
+    <div className="container mx-auto h-screen flex items-center">
+      <motion.div
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
-        transition={{ duration: 1 }}
-        className="container mx-auto px-4 py-6"
+        transition={{ duration: 1, delay: 0.5 }}
+        className="container mx-auto px-4 py-10 mb-10"
       >
-        <Link href="/">
-          <a>
-            <h2 className="font-semibold text-lg">Ismael Amezcua.</h2>
-          </a>
-        </Link>
-      </motion.nav>
-
-      <section className="flex flex-col">
-        <motion.div
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ duration: 1, delay: 0.8 }}
-          className="container mx-auto px-4 py-10 mb-10"
-        >
-          <h1 className="text-2xl leading-relaxed lg:leading-relaxed text-stone-600 cursor-pointer lg:text-4xl">
-            <span className="hover:text-red-600 delay-100 ease-linear transition-colors">
-              The non-unique identifier is Ismael Amezcua.{` `}
-            </span>
+        <h1 className="text-2xl leading-relaxed lg:leading-relaxed text-stone-600 cursor-pointer lg:text-4xl">
+          <span className="hover:text-red-600 delay-100 ease-linear transition-colors">
+            The non-unique identifier is Ismael Amezcua.{` `}
+          </span>
+          <a href="/iav-resume.pdf" download>
             <span className="hover:text-amber-600 delay-100 ease-linear transition-colors">
               I&apos;m a Software Engineer and Computer Scientist from M&eacute;xico.{` `}
             </span>
-            <span className="text-lime-600 hover:text-lime-700 delay-100 ease-linear transition-colors">
-              I really enjoy building great and challenging web applications.{` `}
-            </span>
+          </a>
+          <a>
+            <Link href="/work">
+              <span className="text-lime-600 hover:text-lime-700 delay-100 ease-linear transition-colors">
+                I really enjoy building great and challenging web applications.{` `}
+              </span>
+            </Link>
+          </a>
+          <a href="//www.ucol.mx" target="_blank">
             <span className="hover:text-emerald-600 delay-100 ease-linear transition-colors">
               I teach several computer science courses at Universidad de Colima.{` `}
             </span>
-            <span className="hover:text-cyan-600 delay-100 ease-linear transition-colors">
-              I do research in my spare time on Wireless Sensor Networks, Industrial Internet of Things, communication protocols, and Web technologies.{` `}
-            </span>
-          </h1>
-        </motion.div>
-
-        <motion.div
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ duration: 1, delay: 1.6 }}
-        >
-          <Menu />
-        </motion.div>
-      </section>
-    </>
-  )
+          </a>
+          <a>
+            <Link href="/research">
+              <span className="hover:text-cyan-600 delay-100 ease-linear transition-colors">
+                I do research in my spare time on Wireless Sensor Networks, Industrial Internet of Things, communication protocols, and Web technologies.{` `}
+              </span>
+            </Link>
+          </a>
+        </h1>
+      </motion.div>
+    </div>
+  );
 };
 
 export default Home;
