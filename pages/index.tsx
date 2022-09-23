75cf95cc13ac7a91e0f7e3bc5ee9a42b80c453c6
Ismael Amezcua <amezcua.ismael@gmail.com>
2022-09-23 10:05:35 -0500

    feat(landing): update links and alert for production

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,31 +15,39 @@ const Home: NextPage = () => {
           <span className="hover:text-red-600 delay-100 ease-linear transition-colors">
             The non-unique identifier is Ismael Amezcua.{` `}
           </span>
-          <a href="/iav-resume.pdf" download>
-            <span className="hover:text-amber-600 delay-100 ease-linear transition-colors">
-              I&apos;m a Software Engineer and Computer Scientist from M&eacute;xico.{` `}
-            </span>
-          </a>
+          {/* <a href="/iav-resume.pdf" download> */}
+          <span className="hover:text-amber-600 delay-100 ease-linear transition-colors">
+            I&apos;m a Software Engineer and Computer Scientist from M&eacute;xico.{` `}
+          </span>
+          {/* </a> */}
           <a>
-            <Link href="/work">
-              <span className="text-lime-600 hover:text-lime-700 delay-100 ease-linear transition-colors">
-                I really enjoy building great and challenging web applications.{` `}
-              </span>
-            </Link>
+            {/* <Link href="/work"> */}
+            <span className="text-lime-600 hover:text-lime-700 delay-100 ease-linear transition-colors">
+              I really enjoy building great and challenging web applications.{` `}
+            </span>
+            {/* </Link> */}
           </a>
           <a href="//www.ucol.mx" target="_blank">
             <span className="hover:text-emerald-600 delay-100 ease-linear transition-colors">
               I teach several computer science courses at Universidad de Colima.{` `}
             </span>
           </a>
-          <a>
-            <Link href="/research">
-              <span className="hover:text-cyan-600 delay-100 ease-linear transition-colors">
-                I do research in my spare time on Wireless Sensor Networks, Industrial Internet of Things, communication protocols, and Web technologies.{` `}
-              </span>
-            </Link>
+          <a href="https://scholar.google.com/citations?user=lTngftoAAAAJ" target="_blank">
+            {/* <Link href="/research"> */}
+            <span className="hover:text-cyan-600 delay-100 ease-linear transition-colors">
+              I do research in my spare time on Wireless Sensor Networks, Industrial Internet of Things, communication protocols, and Web technologies.{` `}
+            </span>
+            {/* </Link> */}
           </a>
         </h1>
+      </motion.div>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 1, delay: 1.5 }}
+        className="absolute top-0 left-0 w-full text-center text-slate-600 p-6"
+      >
+        This site is under construction. Comeback sometime in the future for the full experience.
       </motion.div>
     </div>
   );
