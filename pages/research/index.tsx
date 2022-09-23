55f1790eef9d300fa37cefe11e172bea72762be6
Ismael Amezcua <amezcua.ismael@gmail.com>
2022-09-22 23:53:00 -0500

    feat(research): initial commit for the research blog

---
+++ pages/research/index.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from 'next';
+
+const ResearchHome: NextPage = () => {
+  return (
+    <div className="container mx-auto">
+      <h1 className="text-2xl">Protocols&amp;Things</h1>
+    </div>
+  );
+}
+
+export default ResearchHome;
