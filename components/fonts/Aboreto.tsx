b4c2e356779312929a9714eefeedec3a59bc83d4
Ismael Amezcua <amezcua.ismael@gmail.com>
2022-09-25 12:25:33 -0500

    feat(research): add hero section and custom font component

---
+++ components/fonts/Aboreto.tsx
@@ -0,0 +1,12 @@
+import { FC, ReactElement } from 'react';
+import Head from 'next/head';
+
+const AboretoFont: FC = (): ReactElement => (
+  <Head>
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='true' />
+    <link href="https://fonts.googleapis.com/css2?family=Aboreto&display=swap" rel="stylesheet" />
+  </Head>
+);
+
+export default AboretoFont;
--- pages/research/index.tsx
+++ pages/research/index.tsx
@@ -1,32 +1,49 @@
 import { NextPage } from 'next';
 
+import AboretoFont from '../../components/fonts/Aboreto';
+
 const ResearchHome: NextPage = () => {
   return (
-    <div className="container mx-auto py-20">
-      <h1 className="font-black text-6xl text-center text-slate-800 pb-10">
-        Protocols&amp;Things
-      </h1>
+    <>
+      <AboretoFont />
 
-      <div className="grid grid-cols-3 gap-10">
-        <div className="">
-          <img className="rounded-lg" src='https://placeimg.com/640/480/tech' />
-        </div>
-        <div className="col-span-2 flex items-center">
-          <div>
-            <p className="text-sm pb-6">
-              <span className="font-bold">Business, Travel</span>
-              <span> &mdash; July 2, 2022</span>
-            </p>
-            <h1 className="font-black text-4xl text-slate-800 pb-2">
-              Your most unhappy customers are your greatest source of learning.
-            </h1>
-            <p className="text-lg">
-              Far far away, behind the mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right on the coast of the Semantics, a large language ocean.
-            </p>
+      <section className="container mx-auto">
+        {/* Blog title */}
+        <p className="text-center py-20 text-2xl font-aboreto">
+          <span className="font-black text-slate-800">Protocols</span>
+          <span className="text-indigo-600">&amp;</span>
+          <span className="font-black text-slate-800">Things</span>
+        </p>
+
+        {/* Hero section */}
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-10 sm:m-10 bg-gray-100">
+          <div className=" bg-red-100">
+            <img className="w-full h-full" src="https://placeimg.com/720/480/tech" alt="Placeholder" />
+          </div>
+          <div className="flex items-center">
+            <div className="flex flex-col px-8 lg:py-8">
+              <p className="italic pb-2">In <span className="text-indigo-400">Art &amp; Design</span></p>
+              <h1 className="font-aboreto text-3xl sm:text-4xl pb-2">Assure polite his really and others figure though</h1>
+              <p className="text-lg">Its sometimes her behaviour are contented. Do listening am eagerness oh objection collected. Together happy feelings... </p>
+
+              <div className="py-6">
+                <hr className="text-slate-400 pb-4" />
+
+                <a href="/research/articles/1">
+                  <p className="bg-slate-200 text-slate-800 hover:bg-slate-800 hover:text-white hover:fill-white px-3 py-2 inline-flex transition-all duration-500">
+                    <span>Continue reading</span>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                      <path d="M10.707 17.707 16.414 12l-5.707-5.707-1.414 1.414L13.586 12l-4.293 4.293z"></path>
+                    </svg>
+                  </p>
+                </a>
+              </div>
+
+            </div>
           </div>
         </div>
-      </div>
-    </div>
+      </section>
+    </>
   );
 }
 
